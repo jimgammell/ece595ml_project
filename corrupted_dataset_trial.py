cba1d98d7004ad7c447c5dececfd4b92eb1bb34b
Jimmy Ian Gammell <jgammell@ee350lnx13.ecn.purdue.edu>
2022-04-25 10:57:05 -0400

    Finished trials on clean dataset. Some interesting results. Going to try allowing for smaller-magnitude corrections when fewer examples are used.

--- corrupted_dataset_trial.py
+++ corrupted_dataset_trial.py
@@ -225,6 +225,143 @@ class CleanDataset(Dataset):
                                           self.target_transform)
         return validation_dataset
 
+class ImbalancedDatasetTrial:
+    def __init__(self,
+                 method,
+                 train_dataloader,
+                 test_dataloader,
+                 model,
+                 loss_fn,
+                 optimizer,
+                 device,
+                 num_epochs,
+                 batch_size,
+                 evaluate_initial_performance=True,
+                 val_dataloader=None):
+        self.results = Results()
+        self.method = method
+        self.train_dataloader = train_dataloader
+        self.test_dataloader = test_dataloader
+        self.model = model
+        self.loss_fn = loss_fn
+        self.optimizer = optimizer
+        self.device = device
+        self.num_epochs = num_epochs
+        self.batch_size = batch_size
+        self.evaluate_initial_performance = evaluate_initial_performance
+        self.val_dataloader = val_dataloader
+    
+    def __call__(self):
+        if self.evaluate_initial_performance:
+            train_loss, train_acc = self.eval_epoch(0, self.train_dataloader)
+            test_loss, test_acc = self.eval_epoch(0, self.test_dataloader)
+            self.results.update(0, {'train_loss': train_loss,
+                                    'train_acc': train_acc,
+                                    'test_loss': test_loss,
+                                    'test_acc': test_acc})
+        for epoch in range(1, self.num_epochs+1):
+            if self.method == 'naive':
+                train_loss, train_acc = self.naive_train_epoch(epoch)
+                test_loss, test_acc = self.eval_epoch(epoch, self.test_dataloader)
+                self.results.update(epoch, {'train_loss': train_loss,
+                                            'train_acc': train_acc,
+                                            'test_loss': test_loss,
+                                            'test_acc': test_acc})
+            elif self.method == 'ltrwe':
+                res = self.ltrwe_train_epoch(epoch)
+                test_loss, test_acc = self.eval_epoch(epoch, self.test_dataloader)
+                self.results.update(epoch, {'train_loss': res[0],
+                                            'train_acc': res[1],
+                                            'weights_hist': res[2],
+                                            'unused_samples': res[3],
+                                            'p25_weight': res[4],
+                                            'p50_weight': res[5],
+                                            'p75_weight': res[6],
+                                            'mn_weight': res[7],
+                                            'std_weight': res[8],
+                                            'test_loss': test_loss,
+                                            'test_acc': test_acc})
+        return self.results
+        
+    def eval_epoch(self, epoch_num, dataloader):
+        print('Beginning evaluating epoch {}...'.format(epoch_num))
+        batch_losses = []
+        batch_accuracies = []
+        for batch in tqdm(dataloader):
+            images, labels = batch
+            images = images.to(self.device)
+            labels_d = labels.to(self.device)
+            labels = labels.numpy()
+            elementwise_loss, predictions = eval_on_batch(images, labels_d, self.model, self.loss_fn, self.optimizer, self.device)
+            batch_loss = np.mean(elementwise_loss)
+            batch_accuracy = np.mean(np.equal(predictions, labels))
+            batch_losses.append(batch_loss)
+            batch_accuracies.append(batch_accuracy)
+        loss = np.mean(batch_losses)
+        accuracy = np.mean(batch_accuracies)
+        print('\tLoss: {}'.format(loss))
+        print('\tAccuracy: {}'.format(accuracy))
+        return loss, accuracy
+        
+    def naive_train_epoch(self, epoch_num):
+        print('Beginning training epoch {}...'.format(epoch_num))
+        batch_losses = []
+        batch_accuracies = []
+        for batch in tqdm(self.train_dataloader):
+            images, labels = batch
+            images = images.to(self.device)
+            labels_d = labels.to(self.device)
+            labels = labels.numpy()
+            elementwise_loss, predictions = naive_train_on_batch(images, labels_d, self.model, self.loss_fn, self.optimizer, self.device)
+            batch_loss = np.mean(elementwise_loss)
+            batch_accuracy = np.mean(np.equal(predictions, labels))
+            batch_losses.append(batch_loss)
+            batch_accuracies.append(batch_accuracy)
+        loss = np.mean(batch_losses)
+        accuracy = np.mean(batch_accuracies)
+        print('\tLoss: {}'.format(loss))
+        print('\tAccuracy: {}'.format(accuracy))
+        return loss, accuracy
+    
+    def ltrwe_train_epoch(self, epoch_num):
+        print('Beginning training epoch {}...'.format(epoch_num))
+        batch_losses = []
+        batch_accuracies = []
+        batch_weights = []
+        for training_batch in tqdm(self.train_dataloader):
+            training_images, training_labels = training_batch
+            training_images = training_images.to(self.device)
+            training_labels_d = training_labels.to(self.device)
+            training_labels = training_labels.numpy()
+            validation_batch = next(iter(self.val_dataloader))
+            validation_images, validation_labels = validation_batch
+            validation_images = validation_images.to(self.device)
+            validation_labels_d = validation_labels.to(self.device)
+            validation_labels = validation_labels.numpy()
+            elementwise_loss, predictions, weights = ltrwe_train_on_batch(training_images, training_labels_d, validation_images, validation_labels_d, self.model, self.loss_fn, self.optimizer, self.device)
+            batch_loss = np.mean(elementwise_loss)
+            batch_accuracy = np.mean(np.equal(predictions, training_labels))
+            batch_weight = list(weights)
+            batch_losses.append(batch_loss)
+            batch_accuracies.append(batch_accuracy)
+            batch_weights.extend(batch_weight)
+        loss = np.mean(batch_losses)
+        accuracy = np.mean(batch_accuracies)
+        weight = np.histogram(batch_weights, bins=5*self.batch_size, range=(0.0, 1.0))
+        unused_samples = len([w for w in batch_weights if w == 0])
+        med_weight = np.percentile(weights, 50)
+        p25_weight = np.percentile(weights, 25)
+        p75_weight = np.percentile(weights, 75)
+        mn_weight = np.mean(weights)
+        std_weight = np.std(weights)
+        print('\tLoss: {}'.format(loss))
+        print('\tAccuracy: {}'.format(accuracy))
+        print('\tWeight: {}'.format(weight))
+        print('\tUnused samples: {}'.format(unused_samples))
+        print('25/50/75 percentile weights: {} / {} / {}'.format(p25_weight, med_weight, p75_weight))
+        print('Mean/std weights: {} / {}'.format(mn_weight, std_weight))
+        return loss, accuracy, weight, unused_samples, p25_weight, med_weight, p75_weight, mn_weight, std_weight
+    
 class ImbalancedDataset(Dataset):
     def __init__(self,
                  base_dataset,
